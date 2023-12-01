4d8ecce8538d2266035337ba8a76f374ccef70e6
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-01 12:58:18 -0800

    Add _find_distance

--- constant/ch02_taxi/jh/etl.py
+++ constant/ch02_taxi/jh/etl.py
@@ -4,12 +4,16 @@
 
 from pathlib import Path
 
+import geopy.distance
 import pandas as pd
 import sqlalchemy as sa
 import typer
+from ruamel.yaml import YAML
 
 
 class Etl:
+    """Extract, transform, and load Kaggle taxi data into a SQLite trip table."""
+
     def __init__(self, db_file: Path) -> None:
         self.engine = sa.create_engine(f"sqlite:///{db_file}")
 
@@ -18,23 +22,29 @@ class Etl:
         df = pd.read_csv(in_csv, parse_dates=date_cols)
         df = self._discard_unhelpful_columns(df)
         df["distance"] = 0.0
+        self._find_distance(df)  # This costs 6 minutes for 1.46 M rows (4200 row/sec)
+        self._discard_outlier_rows(df)
 
         one_second = "1s"  # trim meaningless milliseconds from observations
         for col in date_cols:
             df[col] = df[col].dt.round(one_second)
 
         with self.engine.begin() as sess:
+            sess.execute(sa.text("DROP TABLE  IF EXISTS  trip"))
             sess.execute(self.ddl)
-        df.to_sql("taxi", self.engine, if_exists="append", index=False)
+        df.to_sql("trip", self.engine, if_exists="append", index=False)
 
-    def _discard_unhelpful_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        self._write_yaml()
+
+    @staticmethod
+    def _discard_unhelpful_columns(df: pd.DataFrame) -> pd.DataFrame:
         df = df.drop(columns=["vendor_id"])  # TPEP: Creative Mobile or Verifone
         df = df.drop(columns=["store_and_fwd_flag"])  # only Y when out-of-range
         return df
 
     ddl = sa.text(
         """
-    CREATE TABLE taxi (
+    CREATE TABLE trip (
         id                 TEXT  PRIMARY KEY,
         pickup_datetime    DATETIME,
         dropoff_datetime   DATETIME,
@@ -48,6 +58,48 @@ class Etl:
     )
     """
     )
+
+    # This is very near both the median pickup and median dropoff point,
+    # Bryant Park behind the lions at the NYPL.
+    grand_central_nyc = 40.752, -73.978
+
+    @staticmethod
+    def _distance(row, center) -> float:
+        return round(
+            geopy.distance.distance(
+                (row.dropoff_latitude, row.dropoff_longitude), center
+            ).m,
+            2,
+        )
+
+    @classmethod
+    def _find_distance(cls, df: pd.DataFrame) -> pd.DataFrame:
+        center = cls.grand_central_nyc
+        df["distance"] = df.apply(lambda row: cls._distance(row, center), axis=1)
+        return df
+
+    @staticmethod
+    def _discard_outlier_rows(df: pd.DataFrame) -> pd.DataFrame:
+        FOUR_HOURS = 4 * 60 * 60  # Somewhat commonly we see 86400 second "trips".
+        # Discard trips where cabbie forgot to turn off the meter.
+        df = df[df.trip_duration < FOUR_HOURS]
+
+        # Discard distant locations, e.g. the Verifone / Automation Anywhere
+        # meter service center in San Jose, CA, locations in the North Atlantic.
+        # SERVICE_RADIUS = 60_000  # 37 miles
+        SERVICE_RADIUS = 140_000  # 87 miles
+        df = df[df.distance < SERVICE_RADIUS]
+
+        assert df.passenger_count.max() <= 9
+        return df
+
+    def _write_yaml(self, out_file="taxi.yml") -> None:
+        d = dict(
+            db_file=self.engine.url.database,
+            grand_central_nyc=self.grand_central_nyc,
+        )
+        yaml = YAML()
+        yaml.dump(d, Path(out_file))
 
 
 def main(in_csv: Path) -> None:
