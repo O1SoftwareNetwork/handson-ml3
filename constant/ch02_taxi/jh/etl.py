2920ae2f3262353e6547c6ad673921ae407a0ec0
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-01 11:45:05 -0800

    Add create_table method

--- constant/ch02_taxi/jh/etl.py
+++ constant/ch02_taxi/jh/etl.py
@@ -1,30 +1,58 @@
-#! /usr/bin/env python
+#! /usr/bin/env SQLALCHEMY_WARN_20=1 python
 # Copyright 2023 O1 Software Network. MIT licensed.
 
 
 from pathlib import Path
 
 import pandas as pd
+import sqlalchemy as sa
 import typer
 
 
-def create_table(in_csv: Path) -> None:
-    date_cols = ["pickup_datetime", "dropoff_datetime"]
-    df = pd.read_csv(in_csv, parse_dates=date_cols)
-    df = _discard_unhelpful_columns(df)
+class Etl:
+    def __init__(self, db_file: Path) -> None:
+        self.engine = sa.create_engine(f"sqlite:///{db_file}")
 
-    one_second = "1s"  # trim meaningless milliseconds from observations
-    for col in date_cols:
-        df[col] = df[col].dt.round(one_second)
+    def create_table(self, in_csv: Path) -> None:
+        date_cols = ["pickup_datetime", "dropoff_datetime"]
+        df = pd.read_csv(in_csv, parse_dates=date_cols)
+        df = self._discard_unhelpful_columns(df)
+        df["distance"] = 0.0
 
-    print(df)
+        one_second = "1s"  # trim meaningless milliseconds from observations
+        for col in date_cols:
+            df[col] = df[col].dt.round(one_second)
+
+        with self.engine.begin() as sess:
+            sess.execute(self.ddl)
+        df.to_sql("taxi", self.engine, if_exists="append", index=False)
+
+    def _discard_unhelpful_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.drop(columns=["vendor_id"])  # TPEP: Creative Mobile or Verifone
+        df = df.drop(columns=["store_and_fwd_flag"])  # only Y when out-of-range
+        return df
+
+    ddl = sa.text(
+        """
+    CREATE TABLE taxi (
+        id                 TEXT  PRIMARY KEY,
+        pickup_datetime    DATETIME,
+        dropoff_datetime   DATETIME,
+        passenger_count    INTEGER,
+        pickup_longitude   FLOAT,
+        pickup_latitude    FLOAT,
+        dropoff_longitude  FLOAT,
+        dropoff_latitude   FLOAT,
+        trip_duration      INTEGER,
+        distance           FLOAT
+    )
+    """
+    )
 
 
-def _discard_unhelpful_columns(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.drop(columns=["vendor_id"])  # TPEP: Creative Mobile or Verifone
-    df = df.drop(columns=["store_and_fwd_flag"])  # only Y when out- {of-town, of-range}
-    return df
+def main(in_csv: Path) -> None:
+    Etl(in_csv.parent / "taxi.db").create_table(in_csv)
 
 
 if __name__ == "__main__":
-    typer.run(create_table)
+    typer.run(main)
