06298e98da87eb23183b06a77ac209fde1b25110
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-05 17:35:42 -0800

    Add add_pickup_dow_hour

--- constant/ch02_taxi/jh/eda_spatial.py
+++ constant/ch02_taxi/jh/eda_spatial.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env streamlit run --server.runOnSave true
+#! /usr/bin/env python
 # Copyright 2023 O1 Software Network. MIT licensed.
 
 import matplotlib.pyplot as plt
@@ -7,12 +7,14 @@ import seaborn as sns
 from beartype import beartype
 
 from constant.ch02_taxi.jh.etl import COMPRESSED_DATASET, Etl
+from constant.ch02_taxi.jh.features import add_pickup_dow_hour
 
 
 @beartype
 def eda(df: pd.DataFrame, num_rows=100_000) -> None:
     etl = Etl(COMPRESSED_DATASET.parent / "taxi.db")
     df = etl.discard_outlier_rows(df)[:num_rows]
+    df = add_pickup_dow_hour(df)
     show_trip_locations(df)
 
 
--- constant/ch02_taxi/jh/features.py
+++ constant/ch02_taxi/jh/features.py
@@ -1,2 +1,12 @@
 #! /usr/bin/env python
 # Copyright 2023 O1 Software Network. MIT licensed.
+"""Feature augmentation."""
+
+
+def add_pickup_dow_hour(df):
+    """Add day-of-week and hour-of-day features."""
+    df["dow"] = df["pickup_datetime"].dt.dayofweek  # Monday=0, Sunday=6
+    df["hour"] = df["pickup_datetime"].dt.hour
+    print(df.dow.describe())
+    print(df.hour.describe())
+    return df
