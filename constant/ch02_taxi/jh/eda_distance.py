702cf186db50c0e453a4b1742736c3d3bd745b31
John Hanley <jhanley708-git@s6.sector6.net>
2023-12-13 11:27:25 -0800

    Add eda_min_time

--- constant/ch02_taxi/jh/eda_distance.py
+++ constant/ch02_taxi/jh/eda_distance.py
@@ -32,8 +32,31 @@ def show_distance_vs_time(df: pd.DataFrame) -> None:
 
 
 @beartype
-def main(in_file: Path = COMPRESSED_DATASET) -> None:
-    eda_distance(pd.read_parquet(in_file))
+def eda_min_time(df: pd.DataFrame) -> None:
+    df["distance_km"] = (df.distance / 1000).apply(int)
+    min_time = df.groupby("distance_km").elapsed.min().reset_index()
+    show_min_time(min_time)
+
+
+@beartype
+def show_min_time(df: pd.DataFrame) -> None:
+    # 13.3 m/s is 30 mph
+    sns.regplot(
+        data=df,
+        x="distance_km",
+        y="elapsed",
+    )
+    plt.show()
+
+
+@beartype
+def main(in_file: Path = COMPRESSED_DATASET, num_rows: int = 100_000) -> None:
+    df = pd.read_parquet(in_file)[:num_rows]
+    df = discard_outlier_rows(df)
+    df = add_pickup_dow_hour(df)
+
+    eda_min_time(df)
+    # eda_distance(pd.read_parquet(in_file))
 
 
 if __name__ == "__main__":
